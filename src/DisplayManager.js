adb2dd48ce2b7b30766650a8dd813eaba636f243
stl <173778947+EnchantmentStealer@users.noreply.github.com>
2024-07-11 15:21:49 +0300

    Forms to add new projects and items

--- src/DisplayManager.js
+++ src/DisplayManager.js
@@ -1,4 +1,6 @@
-import {getAllProjects, getCurrentProject } from "./StorageManager.js"
+import {getAllProjects, getCurrentProject, addNewProject, saveAllProjects, setCurrentProject } from "./StorageManager.js"
+import { addTodoItem } from "./TodoListController.js";
+import TodoItem from "./TodoItem.js";
 
 export function getProjectList() {
   const projects = getAllProjects();
@@ -7,6 +9,10 @@ export function getProjectList() {
   projects.forEach(project => {
     const li = document.createElement("li");
     li.textContent = project.name;
+    li.addEventListener("click", () => {
+      setCurrentProject(project.index);
+      renderTodoList();
+    })
     projectList.appendChild(li);
   })
 
@@ -26,3 +32,44 @@ export function getTodoList() {
 
   return todoList;
 }
+
+export function renderProjects() {
+  const div = document.getElementById("sidebar");
+  div.innerHTML = '';
+  div.appendChild(getProjectList());
+}
+
+export function renderTodoList() {
+  const div = document.getElementById("content");
+  div.innerHTML = "";
+  div.appendChild(getTodoList());
+}
+const projectForm = document.getElementById("newProject");
+projectForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  addNewProject(formData.get("projectName"));
+  renderProjects();
+});
+
+const todoForm = document.getElementById("newItem");
+todoForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const todoItem = new TodoItem(
+    formData.get("title"),
+    formData.get("description"),
+    formData.get("date"),
+    formData.get("priority")
+  )
+  console.log(todoItem);
+  const projects = getAllProjects();
+  const currentProject = getCurrentProject();
+
+  addTodoItem(projects[currentProject].todoList, todoItem);
+  saveAllProjects(projects);
+  renderTodoList();
+})
+
+renderProjects();
+renderTodoList();
